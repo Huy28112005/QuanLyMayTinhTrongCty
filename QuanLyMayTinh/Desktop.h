042336199024c56f383d0d4ba462a432ab67287f
Huỳ8112005 <quochuy2811005@gmail.com>
2024-11-29 18:20:57 +0700

    Add folder QuanLyMayTinh

---
+++ QuanLyMayTinh/Desktop.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "MayTinh.h"
+using namespace toLower;
+
+class Desktop : public MayTinh
+{
+private:
+	string loaiCase;
+public:
+	Desktop() : MayTinh(), loaiCase("") {}
+	Desktop(string loaiCase, unsigned int maMay, string nhanHieu, string hangSanXuat,
+		unsigned short namSanXuat, double giaTien, unsigned int soLuong) : 
+		MayTinh(maMay, nhanHieu, hangSanXuat, namSanXuat, giaTien, soLuong), loaiCase(loaiCase){ }
+	~Desktop() = default;
+	// getter setter
+	void setLoaiCase(const string& loai) { loaiCase = loai; }
+	string getLoaiCase() const { return loaiCase; }
+	// nhap xuat
+	void nhap() {
+		MayTinh::nhap();
+		cin.ignore();
+		cout << "\nNhap loai may tinh (dung / nam): ";
+		getline(cin, loaiCase);
+	}
+	void xuat() const {
+		MayTinh::xuat();
+		cout << "Loai case Desktop: " << loaiCase << rows2;
+		cout << "Thanh tien: " << fixed << setprecision(0) << thanhTien() << " VND" << rows2;
+	}
+	double thanhTien() const {
+		if (toLowerStr(loaiCase) == "dung")
+			return soLuong * giaTien;
+		else if (toLowerStr(loaiCase) == "nam")
+			return (soLuong * giaTien) * 1.05;
+		return 0.0;
+	}
+	Desktop* clone() const override {
+		return new Desktop(*this);
+	}
+};
+
---
+++ QuanLyMayTinh/LapTop.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "MayTinh.h"
+using namespace toLower;
+
+class LapTop : public MayTinh
+{
+private:
+	string mauSac;
+public:
+	LapTop() : MayTinh(), mauSac("") {}
+	LapTop(string mauSac, unsigned int maMay, string nhanHieu, string hangSanXuat,
+		unsigned short namSanXuat, double giaTien, unsigned int soLuong) :
+		MayTinh(maMay, nhanHieu, hangSanXuat, namSanXuat, giaTien, soLuong), mauSac(mauSac) {
+	}
+	~LapTop() = default;
+	// getter setter
+	void setMauSac(const string& mau) { mauSac = mau; }
+	string getMauSac() const { return mauSac; }
+	// nhap xuat
+	void nhap() {
+		MayTinh::nhap();
+		cin.ignore();
+		cout << "\nNhap mau sac Lap top: ";
+		getline(cin, mauSac);
+	}
+	void xuat() const {
+		MayTinh::xuat();
+		cout << "Mau sac LapTop: " << mauSac << rows2;
+		cout << "Thanh tien: " << fixed << setprecision(0) << thanhTien() << " VND" << rows2;
+	}
+	double thanhTien() const {
+		if (toLowerStr(mauSac) == "bac")
+			return (soLuong * giaTien) * 1.03;
+		return soLuong * giaTien;
+	}
+	LapTop* clone() const override {
+		return new LapTop(*this);
+	}
+};
+
